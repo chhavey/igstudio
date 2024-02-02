388f0ab46f07f91a0668b28cec99693969edd6f6
Chhavi <guptachhavi880@gmail.com>
2024-02-02 16:58:32 +0530

    Newsletter page added

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -26,7 +26,7 @@ function FAQ() {
             mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
             officia consequatduis enim velit mollit Exer.
           </p>
-          <Accordion className={styles.accordian}>
+          <Accordion className={styles.accordion}>
             <AccordionSummary
               expandIcon={<Expand />}
               aria-controls="panel1-content"
@@ -35,7 +35,7 @@ function FAQ() {
             >
               How much is my case worth?
             </AccordionSummary>
-            <AccordionDetails className={styles.accordianDetails}>
+            <AccordionDetails className={styles.accordionDetails}>
               The value of your case depends on various factors, including the
               extent of injuries, medical expenses, lost wages, and other
               damages. It's crucial to consult with our experienced attorneys
@@ -43,7 +43,7 @@ function FAQ() {
               evaluation tailored to your specific situation.
             </AccordionDetails>
           </Accordion>
-          <Accordion className={styles.accordian}>
+          <Accordion className={styles.accordion}>
             <AccordionSummary
               expandIcon={<Expand />}
               aria-controls="panel2-content"
@@ -52,7 +52,7 @@ function FAQ() {
             >
               What should I do right after car accident?
             </AccordionSummary>
-            <AccordionDetails className={styles.accordianDetails}>
+            <AccordionDetails className={styles.accordionDetails}>
               After a car accident, prioritize safety and seek medical attention
               for any injuries. Call the police to report the incident and
               exchange information with the other parties involved. Document the
@@ -62,7 +62,7 @@ function FAQ() {
               firm as soon as possible.
             </AccordionDetails>
           </Accordion>
-          <Accordion className={styles.accordian}>
+          <Accordion className={styles.accordion}>
             <AccordionSummary
               expandIcon={<Expand />}
               aria-controls="panel2-content"
@@ -71,7 +71,7 @@ function FAQ() {
             >
               How much does an initial consultation cost?
             </AccordionSummary>
-            <AccordionDetails className={styles.accordianDetails}>
+            <AccordionDetails className={styles.accordionDetails}>
               Our law firm offers a complimentary initial consultation. During
               this meeting, we'll discuss the details of your case, answer any
               questions you may have, and provide an overview of how we can
--- src/pages/Newsletter/NewsLetter.jsx
+++ src/pages/Newsletter/NewsLetter.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./NewsLetter.module.css";
 
 function NewsLetter() {
-  return <div className={styles.container}>NewsLetter</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.newsletterTitle}>Subscribe Our Newsletter</h2>
+        <div className={styles.fields}>
+          <input className={styles.inputName} type="text" placeholder="Name" />
+          <input
+            className={styles.inputEmail}
+            type="text"
+            placeholder="Enter your Email"
+          />
+          <div className={styles.btn}>SEND</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NewsLetter;
